74e30f00d9980522da249896b1bbea9ea1f896d3
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-12-05 01:08:02 +0100

    Day 2 prep

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,8 +1,37 @@
+use std::env;
 mod day1;
+mod day2;
+
+
+fn help() {
+    println!("usage: aoc2020 <integer>
+
+Show solution of day <integer>.");
+}
+
 
 fn main() {
+    // Argument parsing
+    // https://doc.rust-lang.org/stable/rust-by-example/std_misc/arg/matching.html?highlight=argument#argument-parsing
+    let args: Vec<String> = env::args().collect();
+
     println!("Hello, Advent of Code 2020!");
-    day1::solve();
+
+    match args.len() {
+        // one argument passed
+        2 => {
+          match args[1].parse() {
+              Ok(1) => day1::solve(),
+              Ok(2) => day2::solve(),
+              _ => println!("error: solution for that day does not exist."),
+          }
+        },
+        // all the other cases
+        _ => {
+            // show a help message
+            help();
+        }
+    }
 }
 
 
