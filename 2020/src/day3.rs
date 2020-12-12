36978022b7c3f8fe9f1757e70fe786161e82a31a
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-12-12 01:36:49 +0100

    Day 3 part 2

--- 2020/src/day3.rs
+++ 2020/src/day3.rs
@@ -15,28 +15,49 @@ fn test_ride_toboggan() {
 #.##...#...
 #...##....#
 .#..#...#.#";
-    let nb_trees = ride_toboggan(input, 0);
+    let nb_trees = ride_toboggan(input, 0, 3, 1);
     assert_eq!(nb_trees, 7);
+    show_nb_trees(input);
 }
 
 
-fn ride_toboggan(input: &str, start: usize) -> i64 {
+fn ride_toboggan(input: &str, start: usize, right: usize, down: usize) -> i64 {
     let mut nb_trees = 0;
 
     for (n, line) in input.lines().enumerate() {
-        if line.chars().cycle().nth(3*n + start).unwrap() == '#' {
-            // println!("Spotted # at {} {}", 3*n+start, line);
-            nb_trees += 1 ;
+        if (n % down) == 0 {
+            // NOTE: Hack! Only handles 1 and 2
+            let loc = right * n / down + start;
+            if line.chars().cycle().nth(loc).unwrap() == '#' {
+                // println!("Line {} at loc {}: {}", n, loc, line);
+                nb_trees += 1 ;
+            }
         }
     }
     nb_trees
 }
 
+fn show_nb_trees(input: &str) {
+    let mut prod_trees = 1;
+    for (right, down) in vec![(1,1), (3, 1), (5, 1), (7, 1), (1, 2)] {
+      let nb_trees = ride_toboggan(
+          input,
+          0,
+          right,
+          down,
+      );
+      println!(
+          "Number of trees encountered while riding toboggan, right {}, down {} = {}",
+          right,
+          down,
+          nb_trees
+      );
+      prod_trees *= nb_trees;
+    }
+    println!("Product of nb_trees = {}", prod_trees);
+}
+
 pub fn solve() {
-    let nb_trees = ride_toboggan(
-      fs::read_to_string("input/day3").unwrap().as_str(),
-      0
-    );
-    println!("Number of trees encountered while riding toboggan = {}", nb_trees);
+    show_nb_trees(fs::read_to_string("input/day3").unwrap().as_str());
 }
 
